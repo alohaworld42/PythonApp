2f5eec0f69631e8cd6513a52adeb1e1b949bd020
H H <kahiki@hotmail.de>
2024-04-01 14:35:05 +0200

    upload statics and update app

--- schema.sql
+++ schema.sql
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 CREATE TABLE IF NOT EXISTS metadata (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     title TEXT,
@@ -5,3 +6,12 @@ CREATE TABLE IF NOT EXISTS metadata (
     image TEXT,
     url TEXT
 );
+=======
+CREATE TABLE IF NOT EXISTS metadata (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title TEXT,
+    description TEXT,
+    image TEXT,
+    url TEXT
+);
+>>>>>>> 131466d (upload statics and update app)
